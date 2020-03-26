27989acdca71faf30ccb4f96b60cffc6ea6e383e
talented-full-dev <quick525@outlook.com>
2020-03-26 21:18:14 +0300

    Start exploring section background illustrations created

--- components/Sections/SectionBountyPlatform.vue
+++ components/Sections/SectionBountyPlatform.vue
@@ -1,75 +1,79 @@
 <template>
-  <div class="d-container">
-    <div class="w-full flex flex-row flex-wrap justify-center items-center pt-8 pb-12 lg:py-12">
-      <div class="w-full flex flex-col items-center">
-        <!-- Header Text -->
-        <h2
-          class="font-bold text-2xl md:text-3xl text-center leading-tight text-dtPrimary px-6"
-        >{{$t('bountyPlatformSection.header')}}</h2>
-        <!-- Paragraph Text -->
-        <div class="w-full md:w-2/3 xl:w-7/12 flex flex-row justify-center px-4 mt-4 mb-2">
-          <p class="text-lg text-dtText text-center">{{$t('bountyPlatformSection.paragraph')}}</p>
-        </div>
-      </div>
-      <!-- Cards -->
-      <div class="w-full flex flex-row flex-wrap justify-center">
-        <!-- Indicator -->
-        <div class="w-full hidden md:flex flex-col items-center mt-2 px-6">
-          <IllustrationIndicator class="md:w-1/2 lg:w-2/5" />
-        </div>
-        <!-- Post Bounties Card Container -->
-        <div class="w-full md:w-1/2 lg:w-2/5 px-4 py-2 md:pl-6 md:pr-3 mt-3 relative">
-          <!-- Background Illustration Brackets -->
-          <IllustrationBrackets
-            class="hidden md:block md:w-64 lg:w-72 h-auto absolute left-0 bottom-0 md:-ml-24 lg:-ml-32 md:-mb-20 lg:-mb-20 z-0"
-          />
-          <!-- Post Bounties Card -->
-          <div
-            class="w-full flex flex-col bg-dtBackground items-center rounded-tl-xl4 rounded-br-xl4 rounded-tr-xl rounded-bl-xl border-2 border-dtPrimary px-4 py-6 relative"
-          >
-            <!-- Post Bounties Header -->
-            <h3
-              class="font-bold text-xl md:text-2xl text-center leading-tight text-dtPrimary px-6"
-            >{{$t('bountyPlatformSection.postBountyHeader')}}</h3>
-            <!-- Post Bounties Paragraph -->
-            <p
-              class="text-lg text-dtText text-center mt-2 px-4"
-            >{{$t('bountyPlatformSection.postBountyParagraph')}}</p>
-            <!-- Post Bounties Illustration -->
-            <IllustrationPostBounty class="w-3/4 h-auto mt-4" />
-            <!-- Post Bounties Call to Action -->
-            <div class="flex flex-row justify-center">
-              <button
-                class="bg-dtPrimary text-dtText font-extrabold text-xl rounded-tl-xl2 rounded-br-xl2 rounded-tr-md rounded-bl-md px-12 py-2 my-4"
-              >{{ $t("bountyPlatformSection.postBountyButton") }}</button>
-            </div>
+  <div class="overflow-x-hidden">
+    <div class="d-container">
+      <div
+        class="w-full flex flex-row flex-wrap justify-center items-center pt-8 pb-12 lg:pt-12 md:pb-24"
+      >
+        <div class="w-full flex flex-col items-center">
+          <!-- Header Text -->
+          <h2
+            class="font-bold text-2xl md:text-3xl text-center leading-tight text-dtPrimary px-6"
+          >{{$t('bountyPlatformSection.header')}}</h2>
+          <!-- Paragraph Text -->
+          <div class="w-full md:w-2/3 xl:w-7/12 flex flex-row justify-center px-4 mt-4 mb-2">
+            <p class="text-lg text-dtText text-center">{{$t('bountyPlatformSection.paragraph')}}</p>
           </div>
         </div>
-        <!-- Hunt Bounties Card Container -->
-        <div class="w-full md:w-1/2 lg:w-2/5 px-4 py-2 md:pr-6 md:pl-3 mt-3 relative">
-          <!-- Background Illustration Code -->
-          <IllustrationCode
-            class="hidden md:block md:w-56 lg:w-64 h-auto absolute top-0 right-0 md:-mr-32 lg:-mr-40 md:-mt-24 lg:-mt-36 z-0"
-          />
-          <!-- Hunt Bounties Card -->
-          <div
-            class="w-full flex flex-col bg-dtBackground items-center rounded-tl-xl4 rounded-br-xl4 rounded-tr-xl rounded-bl-xl border-2 border-dtPrimary px-4 py-6 relative"
-          >
-            <!-- Hunt Bounties Header -->
-            <h3
-              class="font-bold text-xl md:text-2xl text-center leading-tight text-dtPrimary px-6"
-            >{{$t('bountyPlatformSection.huntBountyHeader')}}</h3>
-            <!-- Hunt Bounties Paragraph -->
-            <p
-              class="text-lg text-dtText text-center mt-2 px-4"
-            >{{$t('bountyPlatformSection.huntBountyParagraph')}}</p>
-            <!-- Hunt Bounties Illustration -->
-            <IllustrationHuntBounty class="w-3/4 h-auto mt-4" />
-            <!-- Hunt Bounties Call to Action -->
-            <div class="flex flex-row justify-center">
-              <button
-                class="bg-dtPrimary text-dtText font-extrabold text-xl rounded-tl-xl2 rounded-br-xl2 rounded-tr-md rounded-bl-md px-12 py-2 my-4"
-              >{{ $t("bountyPlatformSection.huntBountyButton") }}</button>
+        <!-- Cards -->
+        <div class="w-full flex flex-row flex-wrap justify-center">
+          <!-- Indicator -->
+          <div class="w-full hidden md:flex flex-col items-center mt-2 px-6">
+            <IllustrationIndicator class="md:w-1/2 lg:w-2/5" />
+          </div>
+          <!-- Post Bounties Card Container -->
+          <div class="w-full md:w-1/2 lg:w-2/5 px-4 py-2 md:pl-6 md:pr-3 mt-3 relative">
+            <!-- Background Illustration Brackets -->
+            <IllustrationBrackets
+              class="hidden md:block md:w-64 lg:w-72 h-auto absolute left-0 bottom-0 md:-ml-24 lg:-ml-32 md:-mb-20 lg:-mb-20"
+            />
+            <!-- Post Bounties Card -->
+            <div
+              class="w-full flex flex-col bg-dtBackground items-center rounded-tl-xl4 rounded-br-xl4 rounded-tr-xl rounded-bl-xl border-2 border-dtPrimary px-4 py-6 relative"
+            >
+              <!-- Post Bounties Header -->
+              <h3
+                class="font-bold text-xl md:text-2xl text-center leading-tight text-dtPrimary px-6"
+              >{{$t('bountyPlatformSection.postBountyHeader')}}</h3>
+              <!-- Post Bounties Paragraph -->
+              <p
+                class="text-lg text-dtText text-center mt-2 px-4"
+              >{{$t('bountyPlatformSection.postBountyParagraph')}}</p>
+              <!-- Post Bounties Illustration -->
+              <IllustrationPostBounty class="w-3/4 h-auto mt-4" />
+              <!-- Post Bounties Call to Action -->
+              <div class="flex flex-row justify-center">
+                <button
+                  class="bg-dtPrimary text-dtText font-extrabold text-xl rounded-tl-xl2 rounded-br-xl2 rounded-tr-md rounded-bl-md px-12 py-2 my-4"
+                >{{ $t("bountyPlatformSection.postBountyButton") }}</button>
+              </div>
+            </div>
+          </div>
+          <!-- Hunt Bounties Card Container -->
+          <div class="w-full md:w-1/2 lg:w-2/5 px-4 py-2 md:pr-6 md:pl-3 mt-3 relative">
+            <!-- Background Illustration Code -->
+            <IllustrationCode
+              class="hidden md:block md:w-56 lg:w-64 h-auto absolute top-0 right-0 md:-mr-32 lg:-mr-40 md:-mt-24 lg:-mt-36"
+            />
+            <!-- Hunt Bounties Card -->
+            <div
+              class="w-full flex flex-col bg-dtBackground items-center rounded-tl-xl4 rounded-br-xl4 rounded-tr-xl rounded-bl-xl border-2 border-dtPrimary px-4 py-6 relative"
+            >
+              <!-- Hunt Bounties Header -->
+              <h3
+                class="font-bold text-xl md:text-2xl text-center leading-tight text-dtPrimary px-6"
+              >{{$t('bountyPlatformSection.huntBountyHeader')}}</h3>
+              <!-- Hunt Bounties Paragraph -->
+              <p
+                class="text-lg text-dtText text-center mt-2 px-4"
+              >{{$t('bountyPlatformSection.huntBountyParagraph')}}</p>
+              <!-- Hunt Bounties Illustration -->
+              <IllustrationHuntBounty class="w-3/4 h-auto mt-4" />
+              <!-- Hunt Bounties Call to Action -->
+              <div class="flex flex-row justify-center">
+                <button
+                  class="bg-dtPrimary text-dtText font-extrabold text-xl rounded-tl-xl2 rounded-br-xl2 rounded-tr-md rounded-bl-md px-12 py-2 my-4"
+                >{{ $t("bountyPlatformSection.huntBountyButton") }}</button>
+              </div>
             </div>
           </div>
         </div>
--- components/Sections/SectionStartExploring.vue
+++ components/Sections/SectionStartExploring.vue
@@ -1,24 +1,47 @@
 <template>
-  <div class="d-container pt-8 pb-16 md:pt-16 md:pb-32">
-    <div class="w-full flex flex-row flex-wrap justify-center items-center">
-      <div
-        class="w-full md:w-4/5 lg:w-3/5 flex flex-col items-center rounded-tl-xl4 rounded-br-xl4 rounded-tr-xl rounded-bl-xl border-2 border-dtPrimary px-2 md:px-4 py-10 mx-4"
-      >
-        <!-- Header Text -->
-        <h2
-          class="font-bold text-2xl md:text-3xl text-center leading-tight text-dtPrimary px-6 mt-2"
-        >{{$t('startExploringSection.header')}}</h2>
-        <!-- Paragraph Text -->
-        <div class="w-full flex flex-row justify-center px-4 mt-4 mb-2">
-          <p class="text-lg text-dtText text-center">{{$t('startExploringSection.paragraph')}}</p>
-        </div>
-        <!-- Call to Action -->
-        <div class="flex flex-row justify-center">
-          <button
-            class="bg-dtPrimary text-dtText font-extrabold text-xl rounded-tl-xl2 rounded-br-xl2 rounded-tr-md rounded-bl-md px-6 md:px-12 py-2 my-4"
-          >{{ $t("startExploringSection.ctaButton") }}</button>
+  <div class="overflow-x-hidden">
+    <div class="d-container pt-8 pb-16 md:pt-24 md:pb-40 lg:pb-48">
+      <div class="w-full flex flex-row flex-wrap justify-center items-center">
+        <!-- Start Exploring Card Container -->
+        <div class="w-full md:w-4/5 lg:w-3/5 items-center relative">
+          <!-- Background Illustration Compass -->
+          <IllustrationCompass
+            class="hidden md:block md:w-56 lg:w-64 h-auto absolute top-0 right-0 md:-mr-40 lg:-mr-36 md:-mt-20 lg:-mt-20"
+          />
+          <!-- Background Illustration Search -->
+          <IllustrationSearch
+            class="hidden md:block md:w-64 lg:w-72 h-auto absolute left-0 bottom-0 md:-ml-24 lg:-ml-32 md:-mb-24 lg:-mb-32"
+          />
+          <div
+            class="w-full flex flex-col items-center bg-dtBackground rounded-tl-xl4 rounded-br-xl4 rounded-tr-xl rounded-bl-xl border-2 border-dtPrimary px-2 md:px-4 py-10 mx-4 relative"
+          >
+            <!-- Header Text -->
+            <h2
+              class="font-bold text-2xl md:text-3xl text-center leading-tight text-dtPrimary px-6 mt-2"
+            >{{$t('startExploringSection.header')}}</h2>
+            <!-- Paragraph Text -->
+            <div class="w-full flex flex-row justify-center px-4 mt-4 mb-2">
+              <p class="text-lg text-dtText text-center">{{$t('startExploringSection.paragraph')}}</p>
+            </div>
+            <!-- Call to Action -->
+            <div class="flex flex-row justify-center">
+              <button
+                class="bg-dtPrimary text-dtText font-extrabold text-xl rounded-tl-xl2 rounded-br-xl2 rounded-tr-md rounded-bl-md px-6 md:px-12 py-2 my-4"
+              >{{ $t("startExploringSection.ctaButton") }}</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
+<script>
+import IllustrationSearch from "~/components/Illustrations/Background/IllustrationSearch.vue";
+import IllustrationCompass from "~/components/Illustrations/Background/IllustrationCompass.vue";
+export default {
+  components: {
+    IllustrationSearch,
+    IllustrationCompass
+  }
+};
+</script>
