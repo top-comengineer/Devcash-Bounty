272e8a75b5d73feafcc7f098f97563bc5a72e13a
talented-full-dev <quick525@outlook.com>
2020-03-26 20:16:56 +0300

    IBO background illustration added

--- components/Sections/SectionIBO.vue
+++ components/Sections/SectionIBO.vue
@@ -1,26 +1,44 @@
 <template>
-  <div class="d-container">
-    <div
-      class="w-full flex flex-row flex-wrap justify-center lg:justify-around items-center pt-10 pb-10 lg:pt-24 lg:pb-40"
-    >
-      <div class="w-full md:w-4/5 lg:w-2/5 flex flex-col py-8 px-6">
-        <!-- Header Text -->
-        <h2
-          class="font-bold text-2xl md:text-3xl text-center lg:text-left leading-tight text-dtPrimary"
-        >{{$t('IBOSection.header')}}</h2>
-        <!-- Paragraph Text -->
-        <p class="text-lg text-dtText text-center lg:text-left mt-2">{{$t('IBOSection.paragraph')}}</p>
+  <div class="relative overflow-hidden">
+    <!-- Background Illustration Single D -->
+    <IllustrationSingleD
+      class="w-32 md:w-48 lg:w-64 h-auto absolute right-0 top-0 -mr-8 md:-mr-10 lg:-mr-16"
+    />
+    <div class="d-container relative z-10">
+      <div
+        class="w-full flex flex-row flex-wrap justify-center lg:justify-around items-center pt-12 pb-12 md:pt-16 md:pb-16 lg:pt-24 lg:pb-40"
+      >
+        <div class="w-full md:w-4/5 lg:w-2/5 flex flex-col py-8 px-6">
+          <!-- Header Text -->
+          <h2
+            class="font-bold text-2xl md:text-3xl text-center lg:text-left leading-tight text-dtPrimary"
+          >{{$t('IBOSection.header')}}</h2>
+          <!-- Paragraph Text -->
+          <p
+            class="text-lg text-dtText text-center lg:text-left mt-2"
+          >{{$t('IBOSection.paragraph')}}</p>
+        </div>
+        <!-- Illustration -->
+        <IllustrationIBO class="w-10/12 md:w-2/4 lg:w-1/3 order-first lg:order-last" />
       </div>
-      <!-- Illustration -->
-      <IllustrationIBO class="w-10/12 md:w-2/4 lg:w-1/3 order-first lg:order-last" />
     </div>
+    <!-- Background Illustration Cube -->
+    <IllustrationCube
+      class="w-32 md:w-48 lg:w-64 h-auto absolute bottom-0 left-0 -ml-8 md:-ml-10 lg:-ml-16"
+    />
   </div>
 </template>
 <script>
 import IllustrationIBO from "~/components/Illustrations/IllustrationIBO.vue";
+import IllustrationCube from "~/components/Illustrations/Background/IllustrationCube.vue";
+import IllustrationSingleD from "~/components/Illustrations/Background/IllustrationSingleD.vue";
 export default {
   components: {
-    IllustrationIBO
+    IllustrationIBO,
+    IllustrationCube,
+    IllustrationSingleD
   }
 };
 </script>
+<style lang="">
+</style>
