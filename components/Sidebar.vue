034613578cdee9ae78a0b4a9915b78da37954618
talented-full-dev <quick525@outlook.com>
2020-03-27 23:56:44 +0300

    First part of sidebar

---
+++ components/Sidebar.vue
@@ -0,0 +1,86 @@
+<template>
+  <nuxt-link
+    :to="getLocalizedRoute(bountyLink?bountyLink:'bountyplatform')"
+    v-bind:class="[$store.state.theme.dt?'bg-dtBackgroundSecondary':'bg-ltBackgroundSecondary shadow-lg']"
+    class="w-full flex flex-row flex-wrap justify-between items-center py-4 px-6 md:px-8 fill-current rounded-tl-xl3 rounded-br-xl3 rounded-tr-lg rounded-bl-lg transition-all ease-out duration-200"
+  >
+    <!-- Bounty Name and Address -->
+    <div class="w-full md:w-1/2 flex flex-col flex-wrap justify-center items-start">
+      <h4 class="font-extrabold text-xl text-left">{{bountyName}}</h4>
+      <h5 class="font-mono-jet font-medium text-lg text-left mt-1">{{creatorAddress}}</h5>
+    </div>
+    <!-- Divider -->
+    <div
+      v-bind:class="[$store.state.theme.dt? 'bg-dtText': 'bg-ltText']"
+      class="md:hidden w-full h-px rounded-full opacity-5 my-3"
+    ></div>
+    <!-- Submissions Left and Remaining Time -->
+    <div
+      class="w-full md:w-1/4 flex flex-col justify-center order-last md:order-none items-start md:items-end"
+    >
+      <!-- Submissions Left -->
+      <div class="flex flex-row justify-end items-center">
+        <Icon
+          class="w-4 h-4 mr-1"
+          :colorClass="
+                $store.state.theme.dt
+                  ? 'text-dtText'
+                  : 'text-ltText'
+              "
+          type="award"
+        />
+        <h6 class="text-right text-sm">
+          <span class="font-extrabold">{{remainingCount}}</span>
+          <span class="opacity-75">{{$t("bountyPlatform.bountyCard.bountiesLeft")}}</span>
+        </h6>
+      </div>
+      <!-- Remaining Time -->
+      <div class="flex flex-row justify-end items-center mt-1">
+        <Icon
+          class="w-4 h-4 mr-1"
+          :colorClass="
+                $store.state.theme.dt
+                  ? 'text-dtText'
+                  : 'text-ltText'
+              "
+          type="clock"
+        />
+        <h6 class="text-right text-sm">
+          <span class="font-extrabold">{{remainingTime}}</span>
+          <span class="opacity-75">{{$t("bountyPlatform.bountyCard.remaining")}}</span>
+        </h6>
+      </div>
+    </div>
+    <!-- Price in Devcash, Ethereum and Dollars -->
+    <div class="w-full md:w-1/4 flex flex-col justify-center items-start md:items-end">
+      <h4 class="text-dtPrimary font-extrabold text-2xl text-right">{{'{D}' + devAmount}}</h4>
+      <h5 class="text-lg text-right mt-1">{{'Ξ' + ethAmount + ' / ' + '$'+usdAmount}}</h5>
+    </div>
+    <!-- Divider -->
+    <div
+      v-bind:class="[$store.state.theme.dt? 'bg-dtText': 'bg-ltText']"
+      class="md:hidden w-full h-px rounded-full bg-dtText opacity-5 my-3"
+    ></div>
+  </nuxt-link>
+</template>
+<script>
+import Icon from "~/components/Icon.vue";
+export default {
+  components: {
+    Icon
+  },
+  props: {
+    bountyName: null,
+    bountyLink: null,
+    creatorAddress: null,
+    devAmount: null,
+    ethAmount: null,
+    usdAmount: null,
+    remainingCount: null,
+    remainingTime: null
+  },
+  data: function() {
+    return {};
+  }
+};
+</script>
--- pages/bountyplatform/index.vue
+++ pages/bountyplatform/index.vue
@@ -1,99 +1,26 @@
 <template>
-  <div>
-    <div class="d-container">
-      <div class="w-full flex flex-col justify-center items-center py-24 px-4">
-        <BountyCard
-          class="my-2"
-          bountyName="Devcash Logo"
-          creatorAddress="0x60ca...5411"
-          devAmount="1,250,000"
-          ethAmount="2.778"
-          usdAmount="750"
-          remainingCount="1 of 1"
-          remainingTime="3 months"
-        />
-        <BountyCard
-          class="my-2"
-          bountyName="Devcash Logo"
-          creatorAddress="0x60ca...5411"
-          devAmount="1,250,000"
-          ethAmount="2.778"
-          usdAmount="750"
-          remainingCount="1 of 1"
-          remainingTime="3 months"
-        />
-        <BountyCard
-          class="my-2"
-          bountyName="Devcash Logo"
-          creatorAddress="0x60ca...5411"
-          devAmount="1,250,000"
-          ethAmount="2.778"
-          usdAmount="750"
-          remainingCount="1 of 1"
-          remainingTime="3 months"
-        />
-        <BountyCard
-          class="my-2"
-          bountyName="Devcash Logo"
-          creatorAddress="0x60ca...5411"
-          devAmount="1,250,000"
-          ethAmount="2.778"
-          usdAmount="750"
-          remainingCount="1 of 1"
-          remainingTime="3 months"
-        />
-        <BountyCard
-          class="my-2"
-          bountyName="Devcash Logo"
-          creatorAddress="0x60ca...5411"
-          devAmount="1,250,000"
-          ethAmount="2.778"
-          usdAmount="750"
-          remainingCount="1 of 1"
-          remainingTime="3 months"
-        />
-        <BountyCard
-          class="my-2"
-          bountyName="Devcash Logo"
-          creatorAddress="0x60ca...5411"
-          devAmount="1,250,000"
-          ethAmount="2.778"
-          usdAmount="750"
-          remainingCount="1 of 1"
-          remainingTime="3 months"
-        />
-        <BountyCard
-          class="my-2"
-          bountyName="Devcash Logo"
-          creatorAddress="0x60ca...5411"
-          devAmount="1,250,000"
-          ethAmount="2.778"
-          usdAmount="750"
-          remainingCount="1 of 1"
-          remainingTime="3 months"
-        />
-        <BountyCard
-          class="my-2"
-          bountyName="Devcash Logo"
-          creatorAddress="0x60ca...5411"
-          devAmount="1,250,000"
-          ethAmount="2.778"
-          usdAmount="750"
-          remainingCount="1 of 1"
-          remainingTime="3 months"
-        />
-        <BountyCard
-          class="my-2"
-          bountyName="Devcash Logo"
-          creatorAddress="0x60ca...5411"
-          devAmount="1,250,000"
-          ethAmount="2.778"
-          usdAmount="750"
-          remainingCount="1 of 1"
-          remainingTime="3 months"
-        />
-      </div>
+  <div class="w-full flex flex-row relative my-24">
+    <transition name="sidebarTransition">
+      <div
+        v-if="show"
+        class="sidebar bg-dtSecondary fixed my-2 sticky top-0 rounded-tr-xl3 rounded-br-xl3"
+      ></div>
+    </transition>
+    <div class="w-full flex flex-col justify-center items-center px-4">
+      <BountyCard
+        v-for="(n, i) in 10"
+        :key="i"
+        class="my-2"
+        bountyName="Devcash Logo"
+        creatorAddress="0x60ca...5411"
+        devAmount="1,250,000"
+        ethAmount="2.778"
+        usdAmount="750"
+        remainingCount="1 of 1"
+        remainingTime="3 months"
+      />
     </div>
+    <div class="sidebarSpacer"></div>
   </div>
 </template>
 
@@ -104,7 +31,13 @@ export default {
   components: {
     BountyCard
   },
+  data() {
+    return {
+      show: false
+    };
+  },
   mounted: function() {
+    this.show = true;
     /*
     DevcashBounty.init().then((result) => {
 
@@ -112,3 +45,29 @@ export default {
   }
 };
 </script>
+<style>
+.sidebar {
+  max-width: calc((100vw - 1280px) / 2);
+  min-width: 15rem;
+  height: calc(100vh - 8rem);
+}
+.sidebarSpacer {
+  width: calc((100vw - 1280px) / 2);
+}
+.sidebarTransition-enter-active {
+  transition: all 0.25s ease-out;
+  transform-origin: right top;
+}
+.sidebarTransition-leave-active {
+  transition: all 0.25s ease-out;
+  transform-origin: right top;
+}
+.sidebarTransition-enter {
+  opacity: 0;
+  transform: translateX(-20rem);
+}
+.sidebarTransition-leave-to {
+  opacity: 0;
+  transform: translateX(-20rem);
+}
+</style>
