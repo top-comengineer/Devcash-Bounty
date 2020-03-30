a39d2dcbdc76e49effba53bfbe30540079a01141
talented-full-dev <quick525@outlook.com>
2020-03-30 23:31:53 +0300

    Post greeting card

--- components/BountyPlatform/GreetingCard.vue
+++ components/BountyPlatform/GreetingCard.vue
@@ -5,25 +5,42 @@
     "
     class="w-full flex flex-col relative overflow-hidden flex-wrap justify-center items-center bg-dtPrimary py-6 md:py-10 px-6 md:px-8 fill-current rounded-tl-3xl rounded-br-3xl rounded-tr-lg rounded-bl-lg"
   >
+    <!-- Left Background Illustration -->
     <img
+      v-if="type=='post'"
+      class="hidden md:block h-full w-auto absolute left-0"
+      :src="require('~/assets/images/illustrations/background/plus-left.svg')"
+      alt="Plus Left Illustration"
+    />
+    <img
+      v-else
       class="hidden md:block h-full w-auto absolute left-0 ml-4 md:ml-8"
       :src="require('~/assets/images/illustrations/background/explore-left.svg')"
-      alt="Left Illustration"
+      alt="Explore Left Illustration"
     />
+    <!-- Header and Paragraph -->
     <div
       class="w-full md:w-3/4 xl:w-7/12 flex flex-col flex-wrap justify-center items-center mb-2 md:mb-4"
     >
       <h1
         class="text-3xl font-extrabold text-center text-dtText"
-      >{{$t("bountyPlatform.explore.cardHeader")}}</h1>
+      >{{header != null ? header : $t("bountyPlatform.explore.cardHeader")}}</h1>
       <h2
         class="text-md md:text-xl text-center mt-1 text-dtText"
-      >{{$t("bountyPlatform.explore.cardParagraph")}}</h2>
+      >{{paragraph != null ? paragraph : $t("bountyPlatform.explore.cardParagraph")}}</h2>
     </div>
+    <!-- Right Background Illustration -->
     <img
+      v-if="type == 'post'"
+      class="h-full w-auto absolute right-0"
+      :src="require('~/assets/images/illustrations/background/plus-right.svg')"
+      alt="Post Right Illustration"
+    />
+    <img
+      v-else
       class="h-full w-auto absolute right-0 mr-4 md:mr-8"
       :src="require('~/assets/images/illustrations/background/explore-right.svg')"
-      alt="Right Illustration"
+      alt="Explore Right Illustration"
     />
   </div>
 </template>
@@ -34,6 +51,11 @@ export default {
   components: {
     IllustrationExploreLeft,
     IllustrationExploreRight
+  },
+  props: {
+    type: null,
+    header: null,
+    paragraph: null
   }
 };
 </script>
