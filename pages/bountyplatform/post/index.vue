6628c5631efe8f5d88962a7d7311b0dd8e620870
talented-full-dev <quick525@outlook.com>
2020-04-11 13:51:04 +0300

    Bounty Type switch added

--- pages/bountyplatform/post/index.vue
+++ pages/bountyplatform/post/index.vue
@@ -41,11 +41,53 @@
           />
         </div>
       </div>
+      <!-- Card for Bounty Type -->
+      <div
+        :class="[$store.state.theme.dt?'bg-dtBackgroundSecondary':'bg-ltBackgroundSecondary shadow-lg']"
+        class="w-full flex flex-row flex-wrap relative pt-4 pb-8 px-6 md:pt-6 md:pb-10 md:px-10 xl:px-24 mt-1 md:mt-2"
+      >
+        <!-- Bounty Type -->
+        <div class="w-full md:flex-1 flex flex-col my-3">
+          <h3 class="text-xl font-bold px-3">{{$t('bountyPlatform.post.bountyType')}}</h3>
+          <!-- Public and Private Switch -->
+          <div
+            :class="$store.state.theme.dt?'bg-dtBackgroundTertiary border-dtText-10':'bg-ltBackground border-ltText-10'"
+            class="max-w-full w-84 flex flex-row p-1 rounded-full border mt-2"
+          >
+            <div class="w-full flex flex-row relative">
+              <div
+                :class="{'left-0':activeTab=='public', 'left-full -translate-x-full': activeTab=='private', 'shadow-lgSS': $store.state.theme.dt, 'shadow-lgS': !$store.state.theme.dt}"
+                class="absolute w-1/2 h-full w-24 bg-dtPrimary left-0 rounded-full transform transition-all duration-200 ease-out"
+              ></div>
+              <button
+                :class="[activeTab=='public'?'font-bold text-dtText':'font-medium', {'hover_bg-dtText-15 focus_bg-dtText-15': $store.state.theme.dt && activeTab !='public', 'hover_bg-ltText-15 focus_bg-ltText-15': !$store.state.theme.dt && activeTab !='public' }]"
+                @click.prevent="activeTab='public'"
+                class="w-1/2 text-sm md:text-lg leading-tight py-2 px-2 md:px-4 relative truncate rounded-full transition-all duration-300 ease-out"
+              >{{$t('bountyPlatform.post.bountyTypePublic')}}</button>
+              <button
+                :class="[activeTab=='private'?'font-bold text-dtText':'font-medium', {'hover_bg-dtText-15 focus_bg-dtText-15': $store.state.theme.dt && activeTab !='private', 'hover_bg-ltText-15 focus_bg-ltText-15': !$store.state.theme.dt && activeTab !='private' }]"
+                @click.prevent="activeTab='private'"
+                class="w-1/2 text-sm md:text-lg leading-tight py-2 px-2 md:px-4 relative truncate rounded-full transition-all duration-300 ease-out"
+              >{{$t('bountyPlatform.post.bountyTypePrivate')}}</button>
+            </div>
+          </div>
+        </div>
+        <!-- Divider -->
+        <div class="hidden md:block w-16"></div>
+        <!-- Hunter's Address -->
+        <div v-if="activeTab=='private'" class="w-full md:flex-1 flex flex-col my-3">
+          <h3 class="text-xl font-bold px-3">{{$t('bountyPlatform.post.bountyTypeHunterAddress')}}</h3>
+          <input
+            :class="[$store.state.theme.dt?'bg-dtBackgroundTertiary border-dtBackgroundTertiary':'bg-ltBackgroundTertiary border-ltBackgroundTertiary']"
+            class="w-full text-lg font-bold border focus:border-dtPrimary rounded-lg transition-all duration-200 ease-out px-4 py-2 mt-2"
+            type="text"
+            :placeholder="$t('bountyPlatform.post.bountyTypeHunterAddressPlaceholder')"
+          />
+        </div>
+      </div>
       <!-- Card for Number of Bounties and Bounty Amount -->
       <div
-        :class="
-      [$store.state.theme.dt?'bg-dtBackgroundSecondary':'bg-ltBackgroundSecondary shadow-lg']
-    "
+        :class="[$store.state.theme.dt?'bg-dtBackgroundSecondary':'bg-ltBackgroundSecondary shadow-lg']"
         class="w-full flex flex-row flex-wrap relative pt-4 pb-8 px-6 md:pt-6 md:pb-10 md:px-10 xl:px-24 mt-1 md:mt-2"
       >
         <!-- Number of Bounties -->
@@ -86,11 +128,17 @@
         <!-- Deadline -->
         <div class="w-full md:w-1/2 flex flex-col my-3">
           <h3 class="text-xl font-bold px-3">{{$t('bountyPlatform.post.bountyDeadline')}}</h3>
-
-          <div class="w-full flex flex-row">
+          <div class="w-full flex flex-row items-center relative mt-2">
+            <button class="w-8 h-8 absolute top-1/2 bottom-1/2 transform -translate-y-1/2 mx-2">
+              <Icon
+                class="w-full h-full"
+                :colorClass="$store.state.theme.dt?'text-dtText':'text-ltText'"
+                type="calender"
+              />
+            </button>
             <input
               :class="[$store.state.theme.dt?'bg-dtBackgroundTertiary border-dtBackgroundTertiary':'bg-ltBackgroundTertiary border-ltBackgroundTertiary']"
-              class="flex-1 text-lg font-bold border focus:border-dtPrimary rounded-lg transition-all duration-200 ease-out px-4 py-2 mt-2"
+              class="flex-1 text-lg font-bold border focus:border-dtPrimary rounded-lg transition-all duration-200 ease-out pl-12 py-2"
               type="text"
               :placeholder="$t('bountyPlatform.post.bountyDeadlinePlaceholder')"
             />
@@ -143,11 +191,18 @@
 import { SIDEBAR_CONTEXTS } from "~/config";
 import GreetingCard from "~/components/BountyPlatform/GreetingCard.vue";
 import CTACard from "~/components/BountyPlatform/CTACard.vue";
+import Icon from "~/components/Icon.vue";
 export default {
   layout: "bountyPlatform",
   components: {
     GreetingCard,
-    CTACard
+    CTACard,
+    Icon
+  },
+  data() {
+    return {
+      activeTab: "public"
+    };
   },
   beforeMount() {
     // Set sidebar context
