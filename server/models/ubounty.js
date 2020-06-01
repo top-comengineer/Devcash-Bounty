462a32d3a5996bd19f436a5e2ee2bf3e009d9b39
bbedward <bbedward@gmail.com>
2020-06-01 11:55:47 -0400

    Change datatype for description

--- server/models/ubounty.js
+++ server/models/ubounty.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: false
     },
     description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull: false
     },
     contactName: {
