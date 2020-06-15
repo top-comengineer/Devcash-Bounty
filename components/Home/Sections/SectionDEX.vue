066a205410458b4baca130fb98bf40c08e027b9a
talented-full-dev <quick525@outlook.com>
2020-06-15 18:12:36 +0300

    DEX things commented out

--- components/Home/Sections/SectionDEX.vue
+++ components/Home/Sections/SectionDEX.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="overflow-x-hidden">
+  <div class="overflow-x-hidden" id="DEX">
     <div class="d-container pt-12 pb-12 md:pt-20 md:pb-14 lg:pt-32 lg:pb-36">
       <div class="w-full flex flex-row flex-wrap justify-center items-center">
         <div class="w-11/12 md:w-3/4 flex flex-col items-center">
@@ -36,9 +36,9 @@
           <!-- Call to Action -->
           <div class="flex flex-row justify-center relative">
             <button
-              :class="$store.state.theme.dt?'btn-dtPrimary':'btn-ltPrimary'"
-              class="hover_scale-md focus_scale-md bg-dtPrimary text-dtText font-extrabold text-xl rounded-tl-2xl rounded-br-2xl rounded-tr-md rounded-bl-md px-12 py-2 my-4"
-            >{{ $t("home.dexSection.ctaButton") }}</button>
+              disabled
+              class="opacity-50 bg-dtPrimary text-dtText font-extrabold text-xl rounded-tl-2xl rounded-br-2xl rounded-tr-md rounded-bl-md px-12 py-2 my-4"
+            >{{ $t("home.dexSection.buttonComingSoon") }}</button>
           </div>
         </div>
       </div>
