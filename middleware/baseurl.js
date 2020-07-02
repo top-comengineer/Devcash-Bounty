44dbaf7b5c64782fec1e1cf617481cd6b0abc89e
bbedward <bbedward@gmail.com>
2020-07-02 13:15:36 -0400

    Just hard code in URL for now

--- middleware/baseurl.js
+++ middleware/baseurl.js
@@ -1,7 +1,12 @@
 export default function ({ req, route }) {
+    if (process.env.NODE_ENV == 'production'){
+        process.env.CANONICAL_URL = 'https://devcash.appditto.com' + route.path
+        process.env.BASE_URL = 'https://devcash.appditto.com/'
+        return
+    }
     if (process.server) {
-        process.env.CANONICAL_URL = req.protocol + '://' + req.get('x-forwarded-host') ? req.get('x-forwarded-host') : req.get('host') + route.path
-        process.env.BASE_URL = req.protocol + '://' + req.get('x-forwarded-host') ? req.get('x-forwarded-host') : req.get('host')
+        process.env.CANONICAL_URL = req.protocol + '://' + req.get('host') + route.path
+        process.env.BASE_URL = req.protocol + '://' + req.get('host')
         return
     }
     process.env.CANONICAL_URL = window.location.protocol + "//" + window.location.host + route.path
