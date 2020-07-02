c649aa9edef129cb542bb5663597483c2c3415bb
talented-full-dev <quick525@outlook.com>
2020-07-03 00:12:10 +0300

    Error handling additions

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,30 +1,39 @@
 <template>
-  <div class="flex flex-col justify-center items-center pt-12 pb-16 md:pb-32 px-6 text-center">
+  <div class="flex flex-col justify-center items-center pt-4 pb-16 md:pb-32 px-6 text-center">
     <div class="flex flex-row justify-center max-w-full w-132 px-4">
       <img
+        v-if="error.statusCode === 404"
         class="w-full h-auto"
         :src="require('~/assets/images/illustrations/foreground/404.svg')"
         alt="404 Not Found"
       />
+      <img
+        v-else
+        class="w-full h-auto"
+        :src="require('~/assets/images/illustrations/foreground/error.svg')"
+        alt="Error"
+      />
     </div>
-    <p class="text-4xl font-bold mt-4">{{ $t("404.header") }}</p>
-    <p class="text-xl mt-1">{{ $t("404.paragraph") }}</p>
+    <p class="text-4xl font-bold mt-4">{{ $t("errorPage.header") }}</p>
+    <p v-if="error.statusCode === 404" class="text-xl mt-1">{{ $t("errorPage.404paragraph") }}</p>
+    <p v-else class="text-xl mt-1">{{ $t("errorPage.genericParagraph") }}</p>
     <nuxt-link
       class="mt-6 btn-primary bg-c-primary text-c-light transform hover:scale-md focus:scale-md transition-all duration-200 ease-out origin-bottom-left font-extrabold text-xl rounded-tl-2xl rounded-br-2xl rounded-tr-md rounded-bl-md px-8 md:px-12 py-2"
       :to="localePath('/')"
-    >{{ $t("404.buttonCTA") }}</nuxt-link>
+    >{{ $t("errorPage.buttonCTA") }}</nuxt-link>
   </div>
 </template>
 
 <script>
 export default {
-  layout: "404",
+  props: ['error'],
+  layout: "secondary",
   data() {
     return {
       // For meta tags
-      pageTitle: this.$t("meta.404.pageTitle"),
-      pageDescription: this.$t("meta.404.pageDescription"),
-      pagePreview: `${process.env.BASE_URL}/previews/404.png`,
+      pageTitle: this.$t("meta.error.pageTitle"),
+      pageDescription: this.$t("meta.error.pageDescription"),
+      pagePreview: `${process.env.BASE_URL}/previews/error.png`,
       pageThemeColor: "#675CFF",
       canonicalURL: process.env.CANONICAL_URL
     };
