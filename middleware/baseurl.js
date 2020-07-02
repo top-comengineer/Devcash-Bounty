c7afff60a25b2f39d7d25a570d88b1cf58c1dd1f
bbedward <bbedward@gmail.com>
2020-07-02 11:55:04 -0400

    Fix base URL

--- middleware/baseurl.js
+++ middleware/baseurl.js
@@ -1,7 +1,7 @@
 export default function ({ req, route }) {
     if (process.server) {
-        process.env.CANONICAL_URL = req.protocol + '://' + req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.headers.host + route.path
-        process.env.BASE_URL = req.protocol + '://' + req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.headers.host
+        process.env.CANONICAL_URL = req.protocol + '://' + req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.get('host') + route.path
+        process.env.BASE_URL = req.protocol + '://' + req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.get('host')
         return
     }
     process.env.CANONICAL_URL = window.location.protocol + "//" + window.location.host + route.path
