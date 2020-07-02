187b63026efcfba5df4285ec1bcb96520b2b6343
Brandon Berhent <bbedward@gmail.com>
2020-07-02 11:08:28 -0400

    Use x-forwarded-host

--- middleware/baseurl.js
+++ middleware/baseurl.js
@@ -1,7 +1,7 @@
 export default function ({ req, route }) {
     if (process.server) {
-        process.env.CANONICAL_URL = req.protocol + '://' + req.get('host') + route.path
-        process.env.BASE_URL = req.protocol + '://' + req.get('host')
+        process.env.CANONICAL_URL = req.protocol + '://' + req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.headers.host + route.path
+        process.env.BASE_URL = req.protocol + '://' + req.headers['x-forwarded-host'] ? req.headers['x-forwarded-host'] : req.headers.host
         return
     }
     process.env.CANONICAL_URL = window.location.protocol + "//" + window.location.host + route.path
