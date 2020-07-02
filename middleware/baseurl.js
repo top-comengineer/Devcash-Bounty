9247162ac110140ebeb92a77e97f10ad732dff13
Brandon Berhent <bbedward@gmail.com>
2020-07-02 10:57:17 -0400

    Fix env check

--- middleware/baseurl.js
+++ middleware/baseurl.js
@@ -1,7 +1,7 @@
 export default function ({ req, route }) {
     if (process.server) {
-        process.env.CANONICAL_URL = req.protocol + '://' + process.env.NODE_ENV == 'production' ? req.host : req.get('host') + route.path
-        process.env.BASE_URL = req.protocol + '://' + process.env.NODE_ENV == 'production' ? req.host : req.get('host')
+        process.env.CANONICAL_URL = req.protocol + '://' + process.env.NODE_ENV === 'production' ? req.host : req.get('host') + route.path
+        process.env.BASE_URL = req.protocol + '://' + process.env.NODE_ENV === 'production' ? req.host : req.get('host')
         return
     }
     process.env.CANONICAL_URL = window.location.protocol + "//" + window.location.host + route.path
