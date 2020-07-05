ed0075ae02217e83ddebe22118a84f7349de9028
talented-full-dev <quick525@outlook.com>
2020-07-05 14:51:32 +0300

    Preview link change

--- middleware/baseurl.js
+++ middleware/baseurl.js
@@ -1,10 +1,10 @@
 export default function ({ req, route }) {
     if (process.server) {
-        let proto = req.get('x-forwarded-proto') ?  req.get('x-forwarded-proto') : req.protocol
+        let proto = req.get('x-forwarded-proto') ? req.get('x-forwarded-proto') : req.protocol
         let host = req.get('x-forwarded-host') ? req.get('x-forwarded-host') : req.get('host')
         let path = route.path
         process.env.CANONICAL_URL = proto + '://' + host + path
-        process.env.BASE_URL = proto + host
+        process.env.BASE_URL = proto + '://' + host
         return
     }
     process.env.CANONICAL_URL = window.location.protocol + "//" + window.location.host + route.path
