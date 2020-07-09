a539e00d50da004f495b54a4298aba80cad1cdf6
Brandon Berhent <bbedward@gmail.com>
2020-07-09 11:14:35 -0400

    Single env var for postgres

--- server/config/config.js
+++ server/config/config.js
@@ -8,14 +8,10 @@ module.exports = {
     storage: ":memory:"
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
     dialect: 'postgres',
-    //use_env_variable: 'DATABASE_URL',
+    use_env_variable: 'DATABASE_URL',
     pool: {
-      max: 5,
+      max: 10,
       min: 0,
       idle: 10000
     }    
