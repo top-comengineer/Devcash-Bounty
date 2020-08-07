72e4b916e4349dfff5c31cd179eae329289fd4a0
Antoine <fortyonepointsix@gmail.com>
2020-08-06 22:00:48 -0400

    added empty community page

---
+++ pages/community/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    };
+  },
+  head() {
+    return {
+      title: this.pageTitle,
+
+    };
+  }
+};
+</script>
