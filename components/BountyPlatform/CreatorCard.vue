8350ae9ccbd295cadb49983f2bfe07a043eb480d
Wonderful <yunlong870924@gmail.com>
2023-05-15 12:08:26 +0900

    saved on dev1

--- components/BountyPlatform/CreatorCard.vue
+++ components/BountyPlatform/CreatorCard.vue
@@ -1,26 +1,23 @@
 <template>
   <div class="w-full flex flex-col shadow-xl rounded-lg px-4 pt-2 pb-4 border border-c-text-10">
     <div class="flex flex-row flex-wrap items-center">
-      <p
-        class="text-lg font-bold mr-1"
-      >{{!createdFor?$t("bountyPlatform.singleBounty.createdBy"):$t("bountyPlatform.singleBounty.createdFor")}}</p>
-      <button
-        @click.prevent="copyAddress()"
-        class="w-8 h-8 rounded-full hover:bg-c-text-15 focus:bg-c-text-15 p-1 transition-colors duration-200"
-      >
+      <p class="text-lg font-bold mr-1">
+        {{ !createdFor ? $t("bountyPlatform.singleBounty.createdBy") : $t("bountyPlatform.singleBounty.createdFor") }}</p>
+      <button @click.prevent="copyAddress()"
+        class="w-8 h-8 rounded-full hover:bg-c-text-15 focus:bg-c-text-15 p-1 transition-colors duration-200">
         <Icon colorClass="text-c-text" class="w-full h-full" type="copy" />
       </button>
     </div>
     <div class="flex flex-row items-center mt-3">
       <Jazzicon class="flex" :diameter="48" :address="address" />
-      <a class="hover:underline" target="_blank" :href="'https://etherscan.io/address/'+address">
+      <a class="hover:underline" target="_blank" :href="'https://etherscan.io/address/' + address">
         <p v-html="threeLineAddress(address)" class="font-mono-jet text-sm ml-3"></p>
       </a>
     </div>
-    <p v-if="name!=null" class="font-bold mt-3">{{name}}</p>
-    <div v-if="email!=null" class="flex flex-row items-center">
+    <p v-if="name != null" class="font-bold mt-3">{{ name }}</p>
+    <div v-if="email != null" class="flex flex-row items-center">
       <Icon colorClass="text-c-text" class="w-5 h-5" type="email" />
-      <p class="ml-2 text-sm break-all">{{email}}</p>
+      <p class="ml-2 text-sm break-all">{{ email }}</p>
     </div>
   </div>
 </template>
@@ -39,7 +36,7 @@ export default {
     createdFor: Boolean,
   },
   methods: {
-    threeLineAddress: function(address) {
+    threeLineAddress: function (address) {
       return (
         address.substring(0, 14) +
         "<br>" +
@@ -51,12 +48,12 @@ export default {
     copyAddress() {
       this.$copyText(this.address);
       this.$notify({
-          group: 'main',
-          title: this.$t("bountyPlatform.singleBounty.contribute.headerAddressCopied"),
-          text: this.$t("bountyPlatform.singleBounty.contribute.paragraphAddressCopied"),
-          data: {},
-          duration: 1500
-        });
+        group: 'main',
+        title: this.$t("bountyPlatform.singleBounty.contribute.headerAddressCopied"),
+        text: this.$t("bountyPlatform.singleBounty.contribute.paragraphAddressCopied"),
+        data: {},
+        duration: 1500
+      });
     }
   }
 };
